fc873ebca23ff5c6be2a4de8eea7ac460044ce53
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-08-05 15:35:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,60 +8,71 @@ const PREFIX = "/";
 
 client.on('ready', () => {
   console.log('I am ready!');
+  client.user.setActivity('Hentai', { type: 'WATCHING' });
 });
 
 client.on('message', message => {
   if (message.author.bot) return;
 
 if (message.content.toLowerCase().includes("kys nolan")) {
+    console.log("Won't kill myself!");
     message.channel.send('You first, please').then(sentMessage => {
 		sentMessage.delete({timeout:35000});
 	});
   }
   
   if (message.content.toLowerCase().includes("nolan kys")) {
+    console.log("Won't kill myself!");
     message.channel.send('You first, please').then(sentMessage => {
 		sentMessage.delete({timeout:35000});
 	});
   }
 
   if (message.content.toLowerCase().includes("gta 4 pager")) {
+    console.log("GTA 4 Pager!");
     message.channel.send("https://youtu.be/Ee4ATNFER_Y");
   }
   
   if (message.content.toLowerCase().includes("remove lemmon")) {
+    console.log("Remove Lemmon!");
     message.channel.send("https://cdn.discordapp.com/attachments/725409286951403581/726848388523360377/unknown.png");
   }
   
   if (message.content.toLowerCase().includes("omegalemmon")) {
+    console.log("Omegalemmon!");
     message.channel.send("https://cdn.discordapp.com/attachments/725409286951403581/726849685095972914/Png.png");
   }
   
   if (message.content.toLowerCase().includes("kill yourself nolan")) {
+    console.log("Won't kill myself!");
     message.channel.send('You first, please').then(sentMessage => {
 		sentMessage.delete({timeout:35000});
 	});
   }
   
   if (message.content.toLowerCase().includes("nolan kill yourself")) {
+    console.log("Won't kill myself!");
     message.channel.send('You first, please').then(sentMessage => {
 		sentMessage.delete({timeout:35000});
 	});
   }
   
-  if (message.content.toLowerCase().includes("want hate and shame")) {
+  if (message.content.toLowerCase().includes("i want hate and shame")) {
+    console.log("I want hate and shame");
     message.channel.send('I want it too').then(sentMessage => {
 		sentMessage.delete({timeout:35000});
 	});
   }
   
   if (message.content.toLowerCase().includes("animal abuse is not ok")) {
+    console.log("It will never be");
     message.channel.send('It will never be').then(sentMessage => {
 		sentMessage.delete({timeout:35000});
 	});
   }
   
   if (message.content.toLowerCase().includes("animal abuse is not good")) {
+    console.log("It will never be");
     message.channel.send('It will never be').then(sentMessage => {
 		sentMessage.delete({timeout:35000});
 	});
@@ -74,6 +85,7 @@ if (message.content.toLowerCase().includes("kys nolan")) {
   }
 
   if (message.content.toLowerCase().includes("nolan")) {
+    console.log("Nolanized");
     message.channel.send('Nolan').then(sentMessage => {
 		sentMessage.delete({timeout:35000});
 	});
@@ -81,33 +93,40 @@ if (message.content.toLowerCase().includes("kys nolan")) {
 
 
   if (message.content.toLowerCase().includes("lemmon rain")) {
+    console.log("Lemmon Rain!");
     message.channel.send("https://cdn.discordapp.com/attachments/726794126049280064/726794181196120064/unknown-51.png");
   }
   
   if (message.content.toLowerCase().includes("what should i add to the bot")) {
+    console.log("Adding something to the bot!");
     message.channel.send("Something funny ya'know").then(sentMessage => {
 		sentMessage.delete({timeout:35000});
 	});
   }
  
   if (message.content.toLowerCase().includes("real nolan")) {
+    console.log("Ew!");
     message.channel.send('https://cdn.discordapp.com/attachments/725409201492459592/727583476181631056/lmaoface.jpg');
   }
   
   if (message.content.toLowerCase().includes("nolan moment")) {
+    console.log("Nolan Moment");
     message.channel.send('https://media.discordapp.net/attachments/698692718125121556/718032579323887677/final_5ed8bd6d260cea00154600ea_782301.gif');
   }
   
   if (message.content.toLowerCase().includes("bonk")) {
-    message.channel.send('https://cdn.discordapp.com/attachments/686015484281225241/722533493435007066/posterbonk.png');
+	console.log("Hey can somebody keep track of my heads batted in? BONK");
+	message.channel.send('https://cdn.discordapp.com/attachments/686015484281225241/722533493435007066/posterbonk.png');
   }
   
   if (message.content.toLowerCase().includes("detroit become nolan")) {
+    console.log("I have become nolan");
     message.channel.send('https://cdn.discordapp.com/attachments/686015484281225241/722534532615635024/Untitled457_20200616153313.png');
   }
   
   if (message.content.toLowerCase().includes("nolan gets destroyed")) {
-    message.channel.send('https://cdn.discordapp.com/attachments/716123164609871892/722532266835968041/3bsewe.png ');
+	console.log("I got destroyed");
+	  message.channel.send('https://cdn.discordapp.com/attachments/716123164609871892/722532266835968041/3bsewe.png ');
   }
 });
 
