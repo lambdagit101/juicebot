31972ff74afd77ad1a0d68865e1ec0717e2d032e
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-08-07 12:06:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -223,17 +223,17 @@ if (message.content.toLowerCase().startsWith(`${PREFIX}ban`)) {
       	const typeofporn = nsfwarguments[1]; 
 	switch (nsfwarguments[1]) {
 		case 'hentai':
-			randomPuppy("hentai").then(url => {
+			randomPuppy('hentai').then(url => {
 				message.channel.send(url);	
 			});
 		break;
 		case 'tittydrop':
-			randomPuppy("TittyDrop").then(url => {
+			randomPuppy('TittyDrop').then(url => {
 				message.channel.send(url);	
 			});
 		break;
 		case 'porn':
-			randomPuppy("porn").then(url => {
+			randomPuppy('porn').then(url => {
 				message.channel.send(url);	
 			});
 		break;
