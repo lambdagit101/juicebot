26290a57cd82d44ea30b8e40f8f08ec56392b47a
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-08-09 17:04:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -272,7 +272,7 @@ if (message.content.toLowerCase().startsWith(`${PREFIX}ban`)) {
 	 const creditsembed = new MessageEmbed()
       .setTitle('NolanBot')
       .setColor(0xff0000)
-      .setDescription('Made by lambdaguy101 using discord.js. Type /github for the source code');
+      .setDescription('Made by @lambdaguy101#1433 using discord.js. Type /github for the source code');
 	 message.channel.send(creditsembed);
   }
 });
