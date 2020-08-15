35b61a8f42f70b35c4e1abfa0a92afe2366e56f6
SilverCrown <54302245+AstroAntics@users.noreply.github.com>
2020-08-15 23:59:24 +0530

    Updated status_messages
    Added a few new status messages of my own.

--- modules/status_messages.js
+++ modules/status_messages.js
@@ -13,6 +13,10 @@ setInterval(() => {
         { type: 'WATCHING', name: `${guildCount} Servers | /help`},
         { type: 'LISTENING', name: `${userCount} Users | /help`},
         { type: 'WATCHING', name: 'you | /help'}
+        { type: 'WATCHING', name: 'and waiting | /help'}
+        { type: 'WATCHING',name: 'Osana | /help'}
+        //Does this last one work? I've commented it out as a safe feature.
+        //{ type: 'PLAYING', name: 'the Matrix' | /help'}
     ]
 
     client.user.setActivity(statusMessages[index]);
