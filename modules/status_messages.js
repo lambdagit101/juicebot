db54a18c8bfd135cc853071f82c8e0904bc49878
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-08-15 21:56:43 +0300

    Update status_messages.js

--- modules/status_messages.js
+++ modules/status_messages.js
@@ -12,8 +12,8 @@ setInterval(() => {
         { type: 'WATCHING', name: `Hentai | /help`},
         { type: 'WATCHING', name: `${guildCount} Servers | /help`},
         { type: 'LISTENING', name: `${userCount} Users | /help`},
-        { type: 'WATCHING', name: `you | /help'}
-        { type: 'WATCHING', name: `and waiting | /help`}
+        { type: 'WATCHING', name: `You` | /help'},
+        { type: 'WATCHING', name: `and waiting` | /help`},
         { type: 'WATCHING', name: `Osana' | /help`}
         //Does this last one work? I've commented it out as a safe feature.
         //{ type: 'PLAYING', name: `the Matrix | /help'}
