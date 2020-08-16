e8588f7bd8eaba51b68616f5534079a21479dbd5
SilverCrown <54302245+AstroAntics@users.noreply.github.com>
2020-08-16 11:21:27 +0530

    Update moderationCommands.js
    Reformatted some of the code to look more in-line with the rest of the commands.

--- modules/moderationCommands.js
+++ modules/moderationCommands.js
@@ -1,89 +1,98 @@
 //I went ahead and split off the moderation section into its own separate module.
 
 //Kicking a member
-if (message.content.toLowerCase().startsWith(`${PREFIX}kick`)) {
+if (message.content.toLowerCase().startsWith(`${PREFIX}kick`)) 
+{
     const user = message.mentions.users.first();
-    if (user) {
+    if (user) 
+    {
         const member = message.guild.member(user);
-        if (member) {
+        if (member) 
+	{
         member.kick('Kick was requested')
-            .then(() => {
-                const kickembed = new MessageEmbed()
-                    .setTitle('Moderation')
-                    .setColor(0xff0000)
-                    .setDescription(`${user.tag} was successfully kicked!`);
-                message.channel.send(kickembed);
-            })
             
-            .catch(err => {
-                const cantkickembed = new MessageEmbed()
-                    .setTitle('Moderation')
-                    .setColor(0xff0000)
-                    .setDescription("Couldn't kick the user");
-                message.channel.send(cantkickembed);
-                console.error(err);
-            });
+			.then(() => 
+			{
+                	const kickembed = new MessageEmbed()
+                    	.setTitle('Moderation')
+                    	.setColor(0xff0000)
+                    	.setDescription(`${user.tag} was successfully kicked!`);
+                	message.channel.send(kickembed);
+            		})
             
+            		.catch(err => 
+		        {
+                	const cantkickembed = new MessageEmbed()
+                    	.setTitle('Moderation')
+                    	.setColor(0xff0000)
+                    	.setDescription("Couldn't kick the user");
+                	message.channel.send(cantkickembed);
+                	console.error(err);
+            		});
         } 
         
         //You can't kick someone who isn't in the server!
-        else {
+        else 
+	{
             const nomemembed = new MessageEmbed()
             .setTitle('Moderation')
-                .setColor(0xff0000)
-                .setDescription('This user is not in this server');
+            .setColor(0xff0000)
+            .setDescription('This user is not in this server');
             message.channel.send(nomemembed);
         }
     } 
     
     //You have to specify someone!
-    else {
-        const nopersembed = new MessageEmbed()
+    else 
+    {
+	     const nopersembed = new MessageEmbed()
             .setTitle('Moderation')
             .setColor(0xff0000)
             .setDescription('No person was specified!');
-        message.channel.send(nopersembed);
+             message.channel.send(nopersembed);
     }
 }
 	
 //Banning a member.  
-if (message.content.toLowerCase().startsWith(`${PREFIX}ban`)) {
+if (message.content.toLowerCase().startsWith(`${PREFIX}ban`)) 
+{
     const user = message.mentions.users.first();
-    if (user) {
+    if (user) 
+    {
         const member = message.guild.member(user);
-        if (member) {
-            member
-                .ban({
-                    reason: 'Ban was requested',
-                })
+        if (member) 
+	{
+            member.ban({reason: 'Ban was requested',
+        })
                
             .then(() => {
                 const banembed = new MessageEmbed()
-                    .setTitle('Moderation')
-                    .setColor(0xff0000)
-                    .setDescription(`${user.tag} was successfully banned!`);
+                .setTitle('Moderation')
+                .setColor(0xff0000)
+                .setDescription(`${user.tag} was successfully banned!`);
                 message.channel.send(banembed);
             })
             
-            .catch(err => {
-                const cantbanembed = new MessageEmbed()
-                    .setTitle('Moderation')
-                    .setColor(0xff0000)
-                    .setDescription("Couldn't ban the user");
-                message.channel.send(cantbanembed);
-                if (err) console.error(err);
+            .catch(err => 
+	    {
+            const cantbanembed = new MessageEmbed()
+            .setTitle('Moderation')
+            .setColor(0xff0000)
+            .setDescription("Couldn't ban the user");
+            message.channel.send(cantbanembed);
+            if (err) console.error(err);
             });
-            
         } 
         
-        //You can't ban someone who isn't in the server.
-        else {
-            const nobanembed = new MessageEmbed()
-                .setTitle('Moderation')
-                .setColor(0xff0000)
-                .setDescription("This user is not in this server");
-			message.channel.send(nobanembed);
-		}
+      	//You can't ban someone who isn't in the server.
+        else 
+	{
+        const nobanembed = new MessageEmbed()
+        .setTitle('Moderation')
+        .setColor(0xff0000)
+        .setDescription("This user is not in this server");
+	message.channel.send(nobanembed);
+	}
 	}
 }
 
