d5549db34293933aea15efe680a12551ae9ba2c9
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-08-20 23:14:38 +0300

    fixed thing

--- modules/gameCommands.js
+++ modules/gameCommands.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const { client, PREFIX } = require('../index'); // Import client from index.js
 const fetch = require('node-fetch')
 
-client.on('message', message => 
+client.on('message', async (message) => 
 {
     if (message.author.bot) return;
     if (!message.guild) return;
