caea78a7edefec4f8238331634c9af968ca5ce5d
Jan Dickmann <jan@dickmann.tv>
2020-08-20 16:49:47 +0200

    Added firebase thing (NOT FUNCTIONAL YET!!!)

---
+++ firebase_setup.js
@@ -0,0 +1,9 @@
+// Require firebase admin and the service account file.
+var admin = require('firebase-admin');
+const serviceAccount = require('./google_service_key.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
