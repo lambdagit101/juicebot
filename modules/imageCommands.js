c433278be951c16f547984c0e74675c24d1526c0
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-08-21 16:16:03 +0300

    added lorem picsum

--- modules/imageCommands.js
+++ modules/imageCommands.js
@@ -5,17 +5,27 @@ const { client, PREFIX } = require('../index'); // Import client from index.js
 client.on('message', message => 
 {
     if (message.author.bot) return;
-	
-	if (message.content.toLowerCase().startsWith(`${PREFIX}sbubby`)) 
+
+	if (message.content.toLowerCase().startsWith(`${PREFIX}lorempic`)) 
 	{
-		randomPuppy('sbubby').then(url => {
-			message.channel.send(url);
-		});  
-	}
+        const image = await fetch("https://picsum.photos/v2/list?page=2&limit=100");
+        const data = await image.json();
+        var length = data.results.length;
+        var randomNumber = Math.floor(Math.random() * length);
+        var randomImage = data.results[randomNumber];
+        var imeige = randomImage.url;
+        message.channel.send(imeige);
+    }
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}sbubby`)) {
+        randomPuppy('sbubby').then(url => {
+            message.channel.send(url);
+        });
+    }
 	
 	if (message.content.toLowerCase().startsWith(`${PREFIX}animeme`)) 
 	{
-		randomPuppy('Animemes').then(url => {
+        randomPuppy('goodanimemes').then(url => {
 			message.channel.send(url);
 		});  
 	}
