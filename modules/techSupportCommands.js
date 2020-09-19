4e8e5d42fb7e2093525580c8a959ef70d1279188
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-09-20 00:00:58 +0300

    uhh

--- modules/techSupportCommands.js
+++ modules/techSupportCommands.js
@@ -29,8 +29,8 @@ async function techsupport(message) {
         })
         .on("error", (error) => console.error(error));
 	message.channel.send(`Thank you for calling support, my name is ${malenames[Math.random(0, malenames.length)]}, how can I help you?`);
-	message.channel.awaitMessages(m => m.author.id == message.author.id,
-                            {max: 1, time: 30000}).then(collected => {
+	message.channel.awaitMessages(async (m) => m.author.id == message.author.id,
+                            {max: 1, time: 30000}).then( async (collected) => {
                                     const stacksearch = await fetch(`https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&q=${message.content}&site=stackoverflow`);
 									const stackjson = await stacksearch.json();
 									message.channel.send(`This might be a solution: \n${stackjson.items[1].link}`);
