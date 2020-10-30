b98248707acf2f48bb097fb90ff9e773fc6a75b7
M O <cevacinevus@gmail.com>
2020-10-30 13:23:44 +0200

    Update levelingSystem.js

--- modules/levelingSystem.js
+++ modules/levelingSystem.js
@@ -6,43 +6,17 @@ client.on('message', async (message) => {
 	
     if (!message.guild) return;
 	if (message.author.bot) return;
-	xp(message);
+	
+	// Coming soon
 
 	if (!message.content.startsWith(PREFIX)) return;
     if (message.content.toLowerCase().startsWith(`${PREFIX}rank`)) {
 		var user = message.mentions.users.first() || message.author;
-		var level = db.get(`guild_${message.guild.id}_level_${user.id}`) || 0;
-		level = level.toString();
-		let xp = db.get(`guild_${message.guild.id}_xp_${user.id}`) || 0;
-		var xpNeeded = level * 500 + 500;
-		let every = db
-			.all()
-			.filter(1 => 1.ID.startsWith(`guild_${message.guild.id}_xptotal_`))
-			.sort((a, b) => b.data - a.data);
-		var rank = every.map(x => x.10).indexOf(`guild_${message.guild.id}_xptotal_${user.id}`) + 1;
-		rank = rank.toString();
 		const levembed = new Discord.MessageEmbed()
                 .setTitle(`Rank card for ${user.tag}`)
-                .setImage(`https://vacefron.nl/api/rankcard?username=${user.username}&avatar=${user.displayAvatarURL({format: 'png', size: 512})}&level=${level}&rank=0&currentxp=${xp}&nextlevelxp=${xpNeeded}&previouslevelxp=0&xpcolor=#4287f5`)
+                .setImage(`https://vacefron.nl/api/rankcard?username=${user.username}&avatar=${user.displayAvatarURL({format: 'png', size: 512})}&level=${level}&rank=${Math.floor(Math.random() * 10)}&currentxp=${Math.floor(Math.random * 10)}&nextlevelxp=0&previouslevelxp=0&xpcolor=#4287f5`)
                 .setFooter(`Invoked by ${message.author.username}, powered by vacefron.nl`, message.author.avatarURL());
 		return message.channel.send(levembed);
 	}
 	
 });
-
-function xp(message) {
-	if (message.content.startsWith(PREFIX)) return;
-	const randomNumber = Math.floor(Math.random() * 10) + 15;
-	db.add(`guild_${message.guild.id}_xp_${message.author.id}`, randomNumber);
-	db.add(`guild_${message.guild.id}_xptotal_${message.guild.id}, randomNumber`);
-	var level = db.get(`guild_${message.guild.id}_level_${message.author.id}`) || 1;
-	var xp = db.get(`guild_${message.guild.id}_xp_${message.author.id}`);
-	var xpNeeded = level * 500;
-	if (xpNeeded < xp) {
-		var newLevel = db.add(`guild_${message.guild.id}_level_${message.author.id}`, 1);
-		db.subtract(`guild_${message.guild.id}_xp_${message.author.id}`, xpNeeded);
-		message.channel.send(`<@${message.author.id}>, you are now **level ${newLevel}!**`);
-		
-	}
-	
-}
