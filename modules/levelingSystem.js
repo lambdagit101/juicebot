2ee3d8f65dd98ebdf3b79a27150c339ce9d065c0
M O <cevacinevus@gmail.com>
2020-10-30 13:30:57 +0200

    Update levelingSystem.js

--- modules/levelingSystem.js
+++ modules/levelingSystem.js
@@ -10,13 +10,5 @@ client.on('message', async (message) => {
 	// Coming soon
 
 	if (!message.content.startsWith(PREFIX)) return;
-    if (message.content.toLowerCase().startsWith(`${PREFIX}rank`)) {
-		var user = message.mentions.users.first() || message.author;
-		const levembed = new Discord.MessageEmbed()
-                .setTitle(`Rank card for ${user.tag}`)
-                .setImage(`https://vacefron.nl/api/rankcard?username=${user.username}&avatar=${user.displayAvatarURL({format: 'png', size: 512})}&level=${Math.floor(Math.random() * 10)}&rank=${Math.floor(Math.random() * 10)}&currentxp=${Math.floor(Math.random * 10)}&nextlevelxp=0&previouslevelxp=0&xpcolor=#4287f5`)
-                .setFooter(`Invoked by ${message.author.username}, powered by vacefron.nl`, message.author.avatarURL());
-		return message.channel.send(levembed);
-	}
 	
 });
