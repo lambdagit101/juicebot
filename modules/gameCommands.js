ed1e680aa272e9c20297abae8048de61b4166da7
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-10-31 20:44:04 +0200

    Create gameCommands.js

---
+++ modules/gameCommands.js
@@ -0,0 +1,54 @@
+const { client, PREFIX } = require('../index'); // Import the client from index.js
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const prompter = require('discordjs-prompter');
+const db = require('quick.db');
+
+client.on("message", async (message) => {
+
+    if (message.author.bot) return;
+    if (!message.content.startsWith(PREFIX)) return;
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}trivia`)) {
+		var trivia = await fetch('https://opentdb.com/api.php?amount=10&difficulty=medium');
+		var triviajson = await trivia.json();
+		var randomquestion = Math.trunc(Math.random() * 10);
+		const triviaembed = new Discord.MessageEmbed()
+			function addTriviaAnswer(item, index) {
+				triviaembed.addField({ name: 'Possible answer', value: item, inline: true })
+			}
+            .setTitle("Trivia")
+            .setDescription(`${triviajson.results[randomquestion].question}`)
+			triviajson.results[randomquestion].incorrect_answers.forEach(addTriviaAnswer)
+			.addField('Possible answer', triviajson.results[randomquestion].correct_answer, true)
+            .setFooter(`Invoked by ${message.author.username}`, message.author.avatarURL());
+		prompter
+		.message(message.channel, {
+        question: triviaembed,
+        userId: message.author.id,
+        max: 1,
+        timeout: 20000,
+      })
+      .then(responses => {
+        // If no responses, the time ran out
+        if (!responses.size) {
+          return msg.channel.send(`You ran out of time! The answer was ${triviajson.results[randomquestion].correct_answer}`);
+        }
+        // Gets the first message in the collection
+        const response = responses.first();
+ 
+        // Respond
+		if (${response}.toLowerCase().startsWith(triviajson.results[randomquestion].correct_answer)) {
+			var receivedtriviapoints = Math.trunc(Math.random() * 10);
+			db.add(`${message.author.id}_triviapoints`, receivedtriviapointstriviapoints);
+			var usertriviapoints = db.get(`${message.author.id}_triviapoints`);
+			message.channel.send(`Congratulations! You have been awarded ${receivedtriviapoints} trivia points. You now have ${usertriviapoints} trivia points.`);
+		} else {
+			message.channel.send(`Your answer was wrong. The answer was ${triviajson.results[randomquestion].correct_answer}.`);
+		}
+      });
+        return;
+    }
+});
+
+
