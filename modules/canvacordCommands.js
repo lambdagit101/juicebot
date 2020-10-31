ea80ad2d3a8b3e6eaed12302d40f2a746c47cca9
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-10-31 21:56:00 +0200

    j

--- modules/canvacordCommands.js
+++ modules/canvacordCommands.js
@@ -4,23 +4,12 @@ const { client, PREFIX } = require('../index'); // Import client from index.js
 const fetch = require('node-fetch');
 const canvacord = require('canvacord');
 
-// More coming soon! Maybe will make a leveling system with cards using this API
-
+// Coming soon! Maybe will make a leveling system with cards using this API
+/**
 client.on('message', async (message) => 
 {
     if (message.author.bot) return;
 	if (!message.content.startsWith(PREFIX)) return;
 
-    if (message.content.toLowerCase().startsWith(`${PREFIX}phub`)) {
-		const args = message.content.slice(PREFIX.length).trim().split(' ');
-		const username = message.author.username;
-		const profilepicture = message.author.displayAvatarURL({ format: 'png', size: 512 });
-		var command = args.slice(1, args.length);
-		var finalresult = command.join(" ");
-		const image = await canvacord.Canvas.phub({username, finalresult, profilepicture});
-        const attachment = new Discord.MessageAttachment(image.toBuffer(), "phub.png");
-        return message.channel.send(attachment);
-    }
-	
-
 });
+**/
