122aa2efe3d6b9eb03717caa4267ab77b178eb2b
M O <cevacinevus@gmail.com>
2020-11-02 21:09:31 +0200

    Update canvacordCommands.js

--- modules/canvacordCommands.js
+++ modules/canvacordCommands.js
@@ -10,6 +10,20 @@ client.on('message', async (message) =>
 {
     if (message.author.bot) return;
 	if (!message.content.startsWith(PREFIX)) return;
+	
+	if (message.content.toLowerCase().startsWith(`${PREFIX}phub`)) {
+		var username = message.author.username;
+		var pfp = message.author.displayAvatarURL({ format: 'png', size: 512 })
+		const args = message.content.slice(PREFIX.length).trim().split(' ');
+		var text = args.slice(1, args.length);
+		var finalresult = text.join("%20");
+		var image = new canvacord.phub({username, text, pfp});
+		const phubembed = new Discord.MessageEmbed()
+                .setTitle(`${username}, what are you doing here?`)
+                .setImage(image.toBuffer())
+                .setFooter(`Invoked by ${message.author.username}, provided by Canvacord`, message.author.avatarURL());
+            message.channel.send(phubembed);
+	}
 
 });
 **/
