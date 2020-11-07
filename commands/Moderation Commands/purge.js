637e82f8aff6904c59ee130cb244828df4eb4334
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-11-07 10:10:03 +0200

    Update purge.js

--- commands/Moderation Commands/purge.js
+++ commands/Moderation Commands/purge.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { prefix } = require('.../config.json');
 
 module.exports.run = async (client, message, args) => {
     if (message.member.permissions.has('MANAGE_MESSAGES')) {
