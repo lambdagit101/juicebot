a37beee2da769af3282a916b2d2993fc33fc2c5a
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-11-07 18:33:10 +0200

    Update chatbot.js

--- commands/Misc Commands/chatbot.js
+++ commands/Misc Commands/chatbot.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 module.exports.run = async (client, message, args) => {
 		var text = args.slice(0, args.length);
 		var finalresult = text.join("%20");
-		const chatbot = await fetch(`https://api.snowflakedev.xyz/chatbot?message=${finalresult}&name=${require('.../config.json').botname}&gender=MALE&user=${message.author.id}`);
+		const chatbot = await fetch(`https://api.snowflakedev.xyz/chatbot?message=${finalresult}&name=${require('../../config.json').botname}&gender=MALE&user=${message.author.id}`);
 		const messag = await chatbot.json();
 		message.channel.send(messag.message);
 };
