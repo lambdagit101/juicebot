f71c3e9ebc135e3127ef31e622b18aa12524754b
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-11-07 11:34:14 +0200

    Update iplookup.js

--- commands/Misc Commands/iplookup.js
+++ commands/Misc Commands/iplookup.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 const Discord = require('discord.js');
-const { prefix } = require('..../config.json');
+const { prefix } = require('../../config.json');
 
 module.exports.run = async (client, message, args) => {
     const argus = message.content.slice(prefix.length).trim().split(' ');
