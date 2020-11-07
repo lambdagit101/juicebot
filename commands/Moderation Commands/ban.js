13a54613ceae200c2d3f48e48261afee3db0732a
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-11-07 10:08:58 +0200

    Update ban.js

--- commands/Moderation Commands/ban.js
+++ commands/Moderation Commands/ban.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { prefix } = require('.../config.json');
 
 module.exports.run = async (client, message, args) => {
     const user = message.mentions.users.first();
@@ -6,7 +7,7 @@ module.exports.run = async (client, message, args) => {
                 const memberiq = message.guild.member(user);
                 if (memberiq) {
                     if (message.member.permissions.has('BAN_MEMBERS')) {
-                        const args = message.content.slice(PREFIX.length).trim().split(' ');
+                        const args = message.content.slice(prefix.length).trim().split(' ');
                         const text = message.content.split(args[1] + " ")[1];
                         memberiq.ban({ reason: `Banned by ${message.author.tag} from ${message.guild.name}. Reason: ${text}` })
                             .then(() => {
