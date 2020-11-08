2d26c54af29e660ddd4ab10a6998fe57a4c7c2c5
M O <cevacinevus@gmail.com>
2020-11-08 21:10:43 +0200

    Create voteban.js

---
+++ commands/Moderation Commands/voteban.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const Prompter = require('discordjs-prompter');
+const { prefix } = require('../../config.json');
+
+module.exports.run = async (client, message, args) => {
+    const votedperson = message.mentions.users.first();
+	if (message.member.permissions.has('BAN_MEMBERS')) {
+	if (votedperson == message.author) {
+		message.channel.send('You cannot initiate a voteban on yourself.');
+	} else {
+		if (votedperson == null) {
+			message.channel.send('You forgot to specify a person');
+		} else {
+			if (args[1] == null) {
+				message.channel.send('You forgot the vote duration!');
+			} else {
+				Prompter.vote(message.channel, {
+					question: `Ban ${votedperson}?`,
+					choices: ['✅', '❌'],
+					timeout: args[1],
+				}).then((response) => {
+					const winner = response.emojis[0];
+					if (winner.emoji == '✅') {
+						votedperson.ban(`Vote initiated by ${message.author.tag} passed.`);
+						message.channel.send(`${votedperson} has been banned successfully!`);
+					} else {
+						message.channel.send(`Vote failed, ${votedperson} has not been banned.`);
+					}
+				});
+			}
+		}
+	}
+	} else {
+		message.channel.send('You lack permissions!');
+	}
+};
+
+module.exports.help = {
+    name: "voteban",
+    description: "Initiates a vote to ban someone on the server..",
+    aliases: []
+};
