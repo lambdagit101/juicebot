f1d8750d62eddbd93b1b68f730b8287e585443bd
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-11-11 16:48:54 +0200

    Update supreme.js

--- commands/Image Generation Commands/supreme.js
+++ commands/Image Generation Commands/supreme.js
@@ -2,15 +2,12 @@ const Discord = require('discord.js');
 const { prefix } = require('../../config.json');
 
 module.exports.run = async (client, message, args) => {
-		var text = args.slice(0, args.length);
-		var finalresult = text.join("%20");
-
-		const heavenembed = new Discord.MessageEmbed()
+		const richembed = new Discord.MessageEmbed()
 			.setTitle(`${message.author.username} is rich`)
 			.setColor("BLURPLE")
-			.setImage(`https://api.alexflipnote.dev/supreme?text=${finalresult}`)
+			.setImage(`https://api.alexflipnote.dev/supreme?text=${args.join('%20')}`)
 			.setFooter(`Invoked by ${message.author.username}, provided by api.alexflipnote.dev`, message.author.avatarURL());
-			message.channel.send(heavenembed);
+			message.channel.send(richembed);
 };
 
 module.exports.help = {
