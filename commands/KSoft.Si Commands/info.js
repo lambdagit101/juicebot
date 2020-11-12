cbb6f7f2141684c99a845ae2fde06484b521be83
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-11-12 11:09:49 +0200

    Update info.js

--- commands/KSoft.Si Commands/info.js
+++ commands/KSoft.Si Commands/info.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
     let avatar = user.displayAvatarURL({ dynamic: false, format: 'png' });
 		var addinfo = [];
 		const { is_banned } = await fetch(`https://api.ksoft.si/bans/check?user=${args[0]}`, { method: 'get', headers: { 'Authorization': ksoftsikey, 'User-Agent': message.author.id }}).then(response => response.json());
-		if (!guild.member(user.id)) {
+		if (!message.guild.member(user.id)) {
 			addinfo.push('This user is not in the server.') ;
 		};
 		if (is_banned = true) {
