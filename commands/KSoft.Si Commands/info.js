b13cac4bcedd12f3b1dab672e60e5c9ddf620847
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-11-12 10:51:23 +0200

    I been feeling it since 1966, now

--- commands/KSoft.Si Commands/info.js
+++ commands/KSoft.Si Commands/info.js
@@ -6,7 +6,7 @@ module.exports.run = async (client, message, args) => {
 		var user = client.users.cache.find(user => user.id === args[0]) || message.author;
     let avatar = user.displayAvatarURL({ dynamic: false, format: 'png' });
 		var addinfo = [];
-		const { is_banned } = await fetch(`https://api.ksoft.si/bans/check?user${args[0]}`), { method: 'get', headers: { 'Authorization': ksoftsikey, 'User-Agent': message.author.id }}).then(response => response.json());
+		const { is_banned } = await fetch(`https://api.ksoft.si/bans/check?user=${args[0]}`, { method: 'get', headers: { 'Authorization': ksoftsikey, 'User-Agent': message.author.id }}).then(response => response.json());
 		const isinserver = if (!guild.member(user.id)) { addinfo.push('This user is not in the server.') };
 		if (is_banned = true) {
 			addinfo.push('This person has been caught breaking Discord TOS in the past.')
