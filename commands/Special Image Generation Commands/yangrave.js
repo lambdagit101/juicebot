a422dd8cd1bb04bbc5b7b7ca69d7e8dc49601bc3
lambdagit101 <cevacinevus@gmail.com>
2020-11-19 10:45:58 +0200

    fixed yangrave

--- commands/Special Image Generation Commands/yangrave.js
+++ commands/Special Image Generation Commands/yangrave.js
@@ -5,6 +5,8 @@ module.exports.run = async (client, message, args) => {
 		var user = message.mentions.users.first() || message.author;
 		const canvas = Canvas.createCanvas(1280, 720);
 		const ctx = canvas.getContext('2d');
+		const yangrave = await Canvas.loadImage('https://cdn.discordapp.com/attachments/756209304918556723/778899115823071232/2k5szyk9v8x51.png');
+		ctx.drawImage(yangrave, 0, 0, 1280, 720);
 		const avatar = await Canvas.loadImage(user.displayAvatarURL({ format: 'png', size: 512 }));
 		ctx.drawImage(avatar, 736, 168, 450, 450);
     let attachment = new Discord.MessageAttachment(canvas.toBuffer(), "yangrave.png");
