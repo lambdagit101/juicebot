a76f7efed923ccc29e12c5752ef99b3930f638c2
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-11-19 18:28:43 +0200

    Update stats.js

--- commands/Misc Commands/stats.js
+++ commands/Misc Commands/stats.js
@@ -7,8 +7,8 @@ module.exports.run = async (client, message, args) => {
 					.setColor(embedcolor)
 					.setThumbnail('https://cdn.discordapp.com/emojis/773794480134160395.png')
 					.addFields(
-						{ name: 'Total guilds', value: client.users.cache.size, inline: true },
-						{ name: 'Total users', value: client.guilds.cache.size, inline: true },
+						{ name: 'Total guilds', value: client.guilds.cache.size, inline: true },
+						{ name: 'Total users', value: client.users.cache.size, inline: true },
 						{ name: 'Prefix', value: 'jb/', inline: true },
 						{ name: 'Help page', value: '[Click here](https://juicebotdocs.shitpost.business/)' }
 					)
