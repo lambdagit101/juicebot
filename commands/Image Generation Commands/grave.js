ea4c7d3299ebdc5ab5fc9446da9385a987c70894
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-11-29 19:20:23 +0200

    Update grave.js

--- commands/Image Generation Commands/grave.js
+++ commands/Image Generation Commands/grave.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
     const user = message.mentions.users.first() || message.author;
     try {
       const avatarembed = new Discord.MessageEmbed()
-					.setColor("BLURPLE")
+	  .setColor("Rest in peace")
           .setTitle(embedcolor)
           .setImage(`https://vacefron.nl/api/grave?user=${user.displayAvatarURL({ format: 'png', size: 512 })}`)
           .setFooter(`Invoked by ${message.author.username}, provided by vacefron.nl`, message.author.avatarURL());
