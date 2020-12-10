5acc05b297d705d3a749fa7e74712cf5bdf2c1f4
lambdagit101 <cevacinevus@gmail.com>
2020-12-10 19:19:22 +0200

    srtfh

--- index.js
+++ index.js
@@ -41,14 +41,14 @@ client.on("error", console.error);
 
 client.on("message", async (message) => {
 
+    if (message.author.bot || !message.guild) return;
+
     const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; // Min 1, Max 30
     const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
     if (hasLeveledUp) {
       const user = await Levels.fetch(message.author.id, message.guild.id);
       message.channel.send(`${message.author}, congratulations! You have leveled up to **${user.level}**`);
     }
-
-    if (message.author.bot || !message.guild) return;
 
       if (message.content.indexOf(prefix) !== 0) return;
 
