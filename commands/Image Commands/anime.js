e073c166e0db4a1a612df4ac0ebd44551d967b0f
Atul kumar Mishra <63466177+Atul1409@users.noreply.github.com>
2020-12-27 00:47:56 +0530

    Update anime.js

--- commands/Image Commands/anime.js
+++ commands/Image Commands/anime.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const randomanime = require("random-anime");
 
-module.exports.run = (bot, message, args) => {
+module.exports.run = (client, message, args) => {
     const anime = randomanime.anime();
     const embed = new Discord.MessageEmbed().setImage(anime).setColor("BLUE").setTitle(":>").setTimestamp();
     message.channel.send(embed);
