ec52cc8971b5ecdd69eff65741eb8b013bfc701f
Atul kumar Mishra <63466177+Atul1409@users.noreply.github.com>
2020-12-27 00:39:54 +0530

    Create serverInfo.js

---
+++ commands/Misc Commands/serverInfo.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = (bot, message,args) =>{
+        let serverembed = new Discord.MessageEmbed()
+        .setDescription("Information of server:")
+        .setColor('RANDOM')
+        .setThumbnail(message.guild.iconURL)
+        .addField("Name of server:", message.guild.name)
+        .addField("Created at", message.guild.createdAt)
+        .addField("you joined at", message.member.joinedAt)
+        .addField("Total members:", message.guild.memberCount);
+
+        message.channel.send(serverembed);
+}
+
+module.exports.help = {
+    name: "serverinfo",
+    aliases: []
+}
